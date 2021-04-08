6c87fd726b7b6fabf84bc75c5e7f9ffc3a96c273
acbonilla1 <>
2021-04-08 19:46:40 +0000

    commited message

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "acbonilla1"
+git config --global user.email = "acbonilla1@cps.edu"
+git commit -m "commited message"
+git remote add origin https://github.com/acbonilla1/acbonilla1.git
+git push -u origin master
